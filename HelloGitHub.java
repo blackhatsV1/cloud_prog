8f1ba277d5394942f6f09822b515d455ec139fad
yasmine100402 <brimelasting0@gmail.com>
2025-02-10 15:23:52 +0800

    Update HelloGitHub.java

--- HelloGitHub.java
+++ HelloGitHub.java
@@ -1,5 +1,15 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class HelloGitHub {
     public static void main(String[] args) {
-    System.out.println("Hello, GitHub!");
+System.out .println("Hello ,GitHub!");
+
+    LocalDate mydate = LocalDate.now();
+    LocalTime mytime = LocalTime.now();
+    System.out.println("Date: "+ mydate);
+    System.out.println("Time: "+ mytime);
     }
-    }
+}
+    
+    
